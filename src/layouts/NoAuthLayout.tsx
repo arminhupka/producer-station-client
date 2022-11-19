3fd8b9a38d5006645ce9d94857d4aad6c3ea96c2
Armin Hupka <arminhupka@me.com>
2022-11-19 22:55:22 +0100

    -added image to no auth layout

--- src/layouts/NoAuthLayout.tsx
+++ src/layouts/NoAuthLayout.tsx
@@ -1,6 +1,8 @@
 import { Box, Container, Grid, useMediaQuery, useTheme } from "@mui/material";
 import { ReactElement, ReactNode } from "react";
 
+import image from "../assets/images/unauth-bg.jpg";
+
 interface IProps {
   children: ReactNode;
 }
@@ -27,7 +29,11 @@ const NoAuthLayout = ({ children }: IProps): ReactElement => {
         </Grid>
         {!isMobile && (
           <Grid item xs={7} bgcolor='red'>
-            image
+            <Box
+              width='100%'
+              height='100%'
+              sx={{ background: `url(${image}) no-repeat center`, backgroundSize: "cover" }}
+            />
           </Grid>
         )}
       </Grid>
