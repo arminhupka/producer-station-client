c71deb7ee693cd78f31175ad4f11776bb49e361a
Armin Hupka <arminhupka@me.com>
2022-11-24 22:54:27 +0100

    - added logic for adding new label

--- src/components/Modals/NewLabelModal/NewLabelModal.tsx
+++ src/components/Modals/NewLabelModal/NewLabelModal.tsx
@@ -1,25 +1,50 @@
 import { Button, Grid, TextField } from "@mui/material";
+import { AxiosError, AxiosResponse } from "axios";
 import { ReactElement } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useMutation } from "react-query";
+import { useNavigate } from "react-router-dom";
 
+import { Label, NewLabelDto } from "../../../api/api";
+import { ApiError } from "../../../api/apiError";
+import { api } from "../../../utils/api";
 import BaseModal, { IBaseModalProps } from "../BaseModal";
 
 type TProps = Pick<IBaseModalProps, "open" | "onClose">;
 
-const NewLabelModal = ({ onClose, open }: TProps): ReactElement => (
-  <BaseModal title='New Label' onClose={onClose} open={open}>
-    <form>
-      <Grid container gap={2}>
-        <Grid item xs={12}>
-          <TextField label='Label name' fullWidth />
+const NewLabelModal = ({ onClose, open }: TProps): ReactElement => {
+  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm<NewLabelDto>();
+
+  const { mutate, isLoading } = useMutation<AxiosResponse<Label>, AxiosError<ApiError>, NewLabelDto>(
+    async ({ name }) => await api.post<Label>("/labels", { name }, { withCredentials: true }),
+    {
+      onSuccess: ({ data }) => {
+        navigate(`/labels/${data._id}`);
+      },
+    },
+  );
+
+  const handleAddNewLabel: SubmitHandler<NewLabelDto> = (form) => {
+    mutate({ name: form.name });
+  };
+
+  return (
+    <BaseModal title='New Label' onClose={onClose} open={open}>
+      <form onSubmit={handleSubmit(handleAddNewLabel)}>
+        <Grid container gap={2}>
+          <Grid item xs={12}>
+            <TextField label='Label name' fullWidth disabled={isLoading} {...register("name")} />
+          </Grid>
+          <Grid item xs={12}>
+            <Button type='submit' variant='contained' fullWidth disabled={isLoading}>
+              Add New Label
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <Button variant='contained' fullWidth>
-            Add New Label
-          </Button>
-        </Grid>
-      </Grid>
-    </form>
-  </BaseModal>
-);
+      </form>
+    </BaseModal>
+  );
+};
 
 export default NewLabelModal;
